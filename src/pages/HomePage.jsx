40cb875b95cb45375822c1dc08190ae9dacf88e4
jerryxu <13048699388@163.com>
2017-11-21 16:42:26 +0800

    remove the valid code

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,42 +6,17 @@ import echarts from 'echarts';
 import 'echarts/lib/chart/bar';
 import 'echarts/lib/component/tooltip';
 
-@Containerization((state)=>({echart_option_dag:state.PromiseReducer.echart_option_dag}))
+@Containerization()
 export default class HomePage extends React.Component{
 
     state ={
-        _image:{},
-        _data:{
-            nodes:[],
-            nodeRefs:[],
-        },
     }
     componentWillMount(){
-        //dagNodesLoad?startTime=201711101046&endTime=201711101146
-        this.props.dispatch(getAllInstance());
-        this.props.dispatch(dagNodejsLoad());
-    }
-    getOption() {
 
     }
-
-    getData(){
-        const categories = [];
-        const graph = {
-            nodes:[],
-
-        }
-    }
-
     render(){
-
-        const {allInstance,echart_option_dag} = this.props;
         return <div>
-            { echart_option_dag.title ? <ReactEcharts
-                option={echart_option_dag}
-                style={{height: '500px', width: '100%'}}
-            />:''}
-
+            <div>111122</div>
         </div>
     }
 }
