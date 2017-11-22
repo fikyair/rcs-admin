728a411e835727877780bf0999e9fa7fd1f5b197
qiudong <qiu_dong@suixinpay.com>
2017-11-22 14:38:31 +0800

    分页操作

--- src/pages/Pagination.jsx
+++ src/pages/Pagination.jsx
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
-import {Form, Input, Button, Table, Select, Row, Col} from 'antd';
+import {Form} from 'antd';
 import PaginationComponent from '../components/PaginationComponent';
-
-const FormItem = Form.Item;
 
 @Form.create()
 export default class UserList extends Component {
@@ -11,40 +9,36 @@ export default class UserList extends Component {
         pageSize: 20,
         gettingUsers: false,
         users: [],
-        orgName: '',
-        orgNo: [],
         total: 0,
         statusLoading: {},
-        addUser: false,
-        userId: '',
-        UserShow: false,
         queryLoading: false,
     }
 
     search = (args) => {
         const {form: {getFieldsValue}} = this.props;
         const {pageNum, pageSize} = this.state;
-        const orgName = this.state.orgName;
-        const org = this.state.orgNo.find(it => it.orgName === orgName);
-        let orgNo = '';
-        if (org) {
-            orgNo = org.id;
-        }
         let params = {
             ...getFieldsValue(),
             pageNum,
             pageSize,
-            orgNo,
             ...args,
         };
-        this.setState({
-            gettingUsers: true,
-            queryLoading: true,
-        });
-        let data = [{name:'12',age:'23'},{name:'12',age:'23'}]
-        this.setState({users: data})
+        //　此处调用接口
+        // fetch.get('/user/list', params).then(data => {
+        //     console.log(data.list);
+        //     this.setState({
+        //         pageNum: data.pageNum,
+        //         pageSize: data.pageSize,
+        //         total: data.total,
+        //         users: data.list,
+        //     });
+        // }).finally(() => {
+        //     this.setState({
+        //         gettingUsers: false,
+        //         queryLoading: false,
+        //     });
+        // });
     }
-
 
     handlePageNumChange = (pageNum) => {
         this.setState({
@@ -68,63 +62,20 @@ export default class UserList extends Component {
     componentWillMount() {
     }
 
+    componentDidMount() {
+        // 页面渲染完成，进行一次查询
+        this.search();
+    }
+
     render() {
         const {
-            gettingUsers,
-            users,
             total,
             pageNum,
             pageSize,
-            queryLoading,
         } = this.state;
-        const {form: {getFieldDecorator}} = this.props;
-        const formItemLayout = {
-            labelCol: {
-                xs: {span: 24},
-                sm: {span: 8},
-            },
-            wrapperCol: {
-                xs: {span: 24},
-                sm: {span: 16},
-            },
-        };
-        const queryItemLayout = {
-            xs: 12,
-            md: 8,
-            lg: 6,
-        };
         return (
             <div>
-                <Form>
-                    <Row gutter={16}>
-                        <Col {...queryItemLayout}>
-                            <FormItem
-                                {...formItemLayout}
-                                label="登录名称">
-                                {getFieldDecorator('loginName')(
-                                    <Input placeholder="请输入登录名称" style={{width: '100%'}}/>
-                                )}
-                            </FormItem>
-                        </Col>
-                        <Col {...queryItemLayout} style={{display: 'none'}}>
-                            <FormItem
-                                {...formItemLayout}
-                                label="备注名">
-                                {getFieldDecorator('remarkName')(
-                                    <Input placeholder="请输入备注名"/>
-                                )}
-                            </FormItem>
-                        </Col>
-                    </Row>
-                </Form>
-                <Table
-                    loading={gettingUsers}
-                    size="middle"
-                    rowKey={(record) => record.id}
-                    columns={this.columns}
-                    dataSource={users}
-                    pagination={false}
-                />
+
                 <PaginationComponent
                     pageSize={pageSize}
                     pageNum={pageNum}
