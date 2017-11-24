fb7087c1759128f34ce9c5ea3e333c905fec4be8
qiudong <qd123456>
2017-11-24 15:09:57 +0800

    分页操作

--- src/pages/Pagination.jsx
+++ src/pages/Pagination.jsx
@@ -1,90 +1,87 @@
-import React, {Component} from 'react';
-import {Form} from 'antd';
-import PaginationComponent from '../components/PaginationComponent';
+import React from 'react';
+import {FetchAPI} from '../utils/fetch-middleware'
+import {Table, Pagination, Row} from 'antd'
 
-@Form.create()
-export default class UserList extends Component {
+export default class Page extends React.Component {
     state = {
         pageNum: 1,
-        pageSize: 20,
-        gettingUsers: false,
-        users: [],
+        pageSize:3,
         total: 0,
-        statusLoading: {},
-        queryLoading: false,
-    }
-
-    search = (args) => {
-        const {form: {getFieldsValue}} = this.props;
-        const {pageNum, pageSize} = this.state;
-        let params = {
-            ...getFieldsValue(),
-            pageNum,
-            pageSize,
-            ...args,
-        };
-        //　此处调用接口
-        // fetch.get('/user/list', params).then(data => {
-        //     console.log(data.list);
-        //     this.setState({
-        //         pageNum: data.pageNum,
-        //         pageSize: data.pageSize,
-        //         total: data.total,
-        //         users: data.list,
-        //     });
-        // }).finally(() => {
-        //     this.setState({
-        //         gettingUsers: false,
-        //         queryLoading: false,
-        //     });
-        // });
-    }
-
-    handlePageNumChange = (pageNum) => {
-        this.setState({
-            pageNum,
-        });
-        this.search({
-            pageNum,
-        });
-    }
-    handlePageSizeChange = pageSize => {
-        this.setState({
-            pageNum: 1,
-            pageSize,
-        });
-        this.search({
-            pageNum: 1,
-            pageSize,
-        });
+        dataSource: []
     }
 
     componentWillMount() {
+        this.search()
     }
+    columns = [{
+        title: 'ID',
+        dataIndex: 'idCard',
+        key: 'idCard',
+    }, {
+        title: '姓名',
+        dataIndex: 'teaName',
+        key: 'teaName',
+    }, {
+        title: '备注',
+        dataIndex: 'remark',
+        key: 'remark',
+    }];
 
-    componentDidMount() {
-        // 页面渲染完成，进行一次查询
-        this.search();
-    }
 
-    render() {
-        const {
-            total,
+    search(args) {
+        // const {form: {getFieldsValue}} = this.props.form
+        const {pageNum, pageSize} = this.state
+        let params = {
             pageNum,
             pageSize,
-        } = this.state;
+            ...args
+        }
+
+        FetchAPI(`/teacher?pageNum=${params.pageNum}&pageSize=${params.pageSize}`,'GET').then((data)=>{
+            this.setState(
+                {
+                    dataSource:data.records,
+                    total: data.total,
+                    pageSize: data.size,
+                    pageNum: data.current
+
+                }
+            )
+            console.log('====>',data)
+        }).catch((err)=>{
+            console.log(err);
+        })
+
+    }
+
+    onChange = (page) => {
+        this.setState({
+            pageNum: page,
+        });
+        this.search({
+            pageNum: page
+        })
+
+
+    }
+
+    render(){
         return (
             <div>
+                <Table
+                    pagination={false}
+                    dataSource={this.state.dataSource}
+                    columns={this.columns} />
+                <div style={{marginTop: '20px'}}></div>
+                <Row type="flex" justify="end">
+                <Pagination
+                    current={this.state.pageNum}
+                    pageSize={this.state.pageSize}
+                    onChange={this.onChange}
+                    total={this.state.total} />
+                </Row>
 
-                <PaginationComponent
-                    pageSize={pageSize}
-                    pageNum={pageNum}
-                    total={total}
-                    onPageNumChange={this.handlePageNumChange}
-                    onPageSizeChange={this.handlePageSizeChange}
-                />
             </div>
-        );
+        )
     }
 }
-
