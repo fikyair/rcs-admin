c6a358986dd5d774b0e7fd5fc8ce5209c3a6071b
jerryxu <13048699388@163.com>
2017-11-28 14:18:05 +0800

    [封装组件]封装下拉菜单组件

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Containerization } from '../common/PublicComponent';
-import SelectComs, {Option} from '../components/SelectComs'
+import SelectComs, {Option} from '../components/SelectComs';
+import { Layout,Input,Icon} from 'antd'
 
 @Containerization()
 export default class HomePage extends React.Component{
@@ -15,18 +16,15 @@ export default class HomePage extends React.Component{
     render(){
         const {options} =this.state;
         return (
-          <div>
-              <SelectComs labelName="中国" style={{ width:'50%',marginLeft: '25%', marginTop: '20px' }} placeholder={"查询..."} allowClear={true} showSearch={true}>
-                {options}
-              </SelectComs>
-              <div>
+          <Layout>
+            <Input/><Icon type="search" />
               <SelectComs labelName="中国" defaultValue="lucy" style={{ width: 120 }} >
                 <Option value="jack">Jack</Option>
                 <Option value="lucy">Lucy</Option>
                 <Option value="disabled" disabled>Disabled</Option>
               </SelectComs>
-              </div>
-            </div>
+
+            </Layout>
         );
     }
 }
