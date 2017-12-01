c3561d80c283d1add037b5e1e6541169b3881510
shiming <xue_sm@suixingpay.com>
2017-12-01 18:10:35 +0800

    页面完善

--- src/pages/limit/LimitDetails.jsx
+++ src/pages/limit/LimitDetails.jsx
@@ -45,6 +45,57 @@ export default class LimitDetails extends React.Component {
             labelName: 'POS结算周期',
             labelValue: 'posbalanceTime',
         }],
+        transactionAttrData:[{
+            labelName: '卡属性',
+            labelValue: 'carType',
+        },{
+            labelName: '卡介质',
+            labelValue: 'media'
+        },{
+            labelName: '消费方式',
+            labelValue: 'consumeType'
+        },{
+            labelName: '接触方式',
+            labelValue: 'contactType'
+        }],
+        scanData:[{
+                labelName: '扫码类型',
+                labelValue: 'scanType'
+        }],
+        modifyLimitData:[{
+            labelName: '单笔',
+            labelValue: 'sigle',
+            addonBefore: "金额",
+            addonAfter: "元"
+        },
+            {
+                labelName: '单日',
+                labelValue: 'sigle',
+                addonBefore: "金额",
+                addonAfter: "元"
+            }, {
+                labelName: '单月',
+                labelValue: 'sigle',
+                addonBefore: "金额",
+                addonAfter: "元"
+            },
+            {
+                labelName: '终身',
+                labelValue: 'sigle',
+                addonBefore: "金额",
+                addonAfter: "元"
+            }, {
+                labelName: '两笔间隔',
+                labelValue: 'sigle',
+                addonAfter: "元"
+            },
+            {
+                labelName: '笔／日',
+                labelValue: 'sigle',
+                addonAfter: "元"
+            }
+
+        ]
     }
 
     render() {
@@ -60,12 +111,17 @@ export default class LimitDetails extends React.Component {
             },
         };
         const queryItemLayout = {
-            xs: 12,
-            sm: 8,
-            md: 6,
-        }
+            xs: 12, //手机
+            sm: 7,   //平板
+            md: 5,   //pc
+        };
+        const tailItemLayout = {
+            xs: 24,
+            sm: 24,    //平板的时候占一行，因为一行有24格
+            md:1       //在PC上的时候分一格  ，剩下的23格input组件来分
+        };
         return (
-            <div style={{margin: '15px 35px '}}>
+            <div >
                 <div style={{textAlign: 'center', marginBottom: 10, marginTop: 10, fontSize: 16}} > <b>限额名称：POS商户对私结算限额</b> </div>
                 <Form >
                     <Card title="商户属性：" bordered={true}>
@@ -90,92 +146,51 @@ export default class LimitDetails extends React.Component {
                 <Form>
                     <Card title="交易属性" border={true}>
                         <Row style={{marginTop: 10}}>
-                            <Col span={2}>
-                                线下交易：
+                            <Col {...tailItemLayout}
+                            >
+                                <span style={{marginTop: 16, display: 'inline-block'}}> 线下交易:</span>
                             </Col>
+                            <Col>
+                                {
+                                        this.initData.transactionAttrData.map((v,k)=>{
 
-                            <Col span={6}>
-                                <FormItem
-                                    {...formItemLayout}
-                                    label="卡属性："
-                                >
+                                            return(
+                                                <Col {...queryItemLayout}>
+                                                    {
+                                                        getFieldDecorator(v.labelValue)(
+                                                            <InputComs  disabled={true} labelName={v.labelName} style={{width: 120}}/>
+                                                        )
+                                                    }
+                                                </Col>
 
-                                    {getFieldDecorator('carType')(
-                                        <Input disabled={true}/>
-                                    )}
-                                </FormItem>
-                            </Col>
-                            <Col span={6}>
-                                <FormItem
-                                    {...formItemLayout}
-                                    label="卡介质："
-                                >
+                                                )
+                                        })
+                                }
 
-                                    {getFieldDecorator('media')(
-                                        <Input disabled={true}/>
-                                    )}
-                                </FormItem>
-                            </Col>
-                            <Col span={5}>
-                                <FormItem
-                                    {...formItemLayout}
-                                    label="消费方式："
-                                >
-
-                                    {getFieldDecorator('consumeType')(
-                                        <Input disabled={true}/>
-                                    )}
-                                </FormItem>
-                            </Col>
-                            <Col span={5}>
-                                <FormItem
-                                    {...formItemLayout}
-                                    label="接触方式："
-                                >
-
-                                    {getFieldDecorator('contactType')(
-                                        <Input disabled={true}/>
-                                    )}
-                                </FormItem>
                             </Col>
                         </Row>
-
-                        <Row style={{marginTop: 10}}>
-                            <Col span={2}>
-                                扫码交易：
+                        <Row >
+                            <Col {...tailItemLayout}
+                            >
+                                <span style={{marginTop: 16, display: 'inline-block'}}> 扫码交易:</span>
                             </Col>
-                            <Col span={6}>
-                                <FormItem
-                                    {...formItemLayout}
-                                    label="扫码类型："
-                                >
+                            <Col>
+                                {
+                                    this.initData.scanData.map((v,k)=>{
 
-                                    {getFieldDecorator('scanType')(
-                                        <Input disabled={true}/>
-                                    )}
-                                </FormItem>
-                            </Col>
-                            <Col span={6}>
-                                <FormItem
-                                    {...formItemLayout}
-                                    label="扫码类型："
-                                >
+                                        return(
+                                            <Col {...queryItemLayout}>
+                                                {
+                                                    getFieldDecorator(v.labelValue)(
+                                                        <InputComs  disabled={true} labelName={v.labelName} style={{width: 120}}/>
+                                                    )
+                                                }
+                                            </Col>
 
-                                    {getFieldDecorator('scanType')(
-                                        <Input disabled={true}/>
-                                    )}
-                                </FormItem>
-                            </Col>
-                            <Col span={5}>
-                                <FormItem
-                                    {...formItemLayout}
-                                    label="扫码类型："
-                                >
+                                        )
+                                    })
+                                }
 
-                                    {getFieldDecorator('scanType')(
-                                        <Input disabled={true}/>
-                                    )}
-                                </FormItem>
                             </Col>
                         </Row>
                     </Card>
@@ -183,89 +198,46 @@ export default class LimitDetails extends React.Component {
                 <Form>
                     <Card title={"修改限额值"} bordered={true}>
                     <Row style={{marginTop: 10}}>
-                        <Col span={6}>
-                            <FormItem
-                                {...formItemLayout}
-                                label="单笔(金额)："
-                            >
+                        {
+                            this.initData.modifyLimitData.map((v, k) => {
 
-                                {getFieldDecorator('singleAmount')(
-                                    <Input addonAfter={'元'} disabled={true}/>
-                                )}
-                            </FormItem>
-                        </Col>
-                        <Col span={6}>
-                            <FormItem
-                                {...formItemLayout}
-                                label="单日(金额)："
-                            >
-
-                                {getFieldDecorator('singleAmountDay')(
-                                    <Input addonAfter={'元'} disabled={true}/>
-                                )}
-                            </FormItem>
-                        </Col>
-                        <Col span={6}>
-                            <FormItem
-                                {...formItemLayout}
-                                label="单月(金额)："
-                            >
-
-                                {getFieldDecorator('singleAmountMonth')(
-                                    <Input addonAfter={'元'} disabled={true}/>
-                                )}
-                            </FormItem>
-                        </Col>
-                        <Col span={6}>
-                            <FormItem
-                                {...formItemLayout}
-                                label="终身(金额)："
-                            >
-
-                                {getFieldDecorator('lifeAmountDay')(
-                                    <Input addonAfter={'元'} disabled={true}/>
-                                )}
-                            </FormItem>
-                        </Col>
-                    </Row>
-
-                    <Row style={{marginTop: 10}}>
-                        <Col span={6}>
-                            <FormItem
-                                {...formItemLayout}
-                                label="两笔间隔："
-                            >
-
-                                {getFieldDecorator('scanType')(
-                                    <Input addonAfter={'秒'} disabled={true}/>
-                                )}
-                            </FormItem>
-                        </Col>
-                        <Col span={6}>
-                            <FormItem
-                                {...formItemLayout}
-                                label="笔数/分钟："
-                            >
-
-                                {getFieldDecorator('countByMinute')(
-                                    <InputGroup compact >
-                                        <Input disabled={true} style={{width: 65}}addonAfter={'笔'} />
-                                        <Input style={{width: 20, borderLeft: 0, pointerEvents: 'none', backgroundColor: '#fff' }} placeholder="/" disabled />
-                                        <Input disabled={true} style={{width: 65, borderLeft: 0}} addonAfter={'分'} />
-                                    </InputGroup>
-                                )}
-                            </FormItem>
-                        </Col>
-
-                        <Col span={6}>
-                            <FormItem
-                                {...formItemLayout}
-                                label="笔/日"
-                            >
-                                {getFieldDecorator('minute')(
-                                    <Input addonAfter={'笔'} disabled={true}/>
-                                )}
-                            </FormItem>
+                                    return (
+                                        <Col {...queryItemLayout}>
+                                            {
+                                                getFieldDecorator(v.labelValue)(
+                                                    <InputComs disabled={true} labelName={v.labelName} style={{width: 120}}
+                                                               addonBefore={v.addonBefore} addonAfter={v.addonAfter}/>
+                                                )
+                                            }
+                                        </Col>
+                                    )
+                                }
+                            )
+                        }
+                        <Col {...queryItemLayout}>
+                            <Row style={{marginTop: '10px'}}>
+                                <Col span={7}>
+                                    <span style={{marginTop: '5px', display: 'inline-block'}}>每笔／分钟:</span>
+                                </Col>
+                                <Col span={16}>
+                                    {
+                                        getFieldDecorator('limitType')(
+                                            <InputGroup>
+                                                <Input disabled={true} style={{width: 60, textAlign: 'center'}}
+                                                       />
+                                                <Input style={{
+                                                    width: 24,
+                                                    borderLeft: 0,
+                                                    pointerEvents: 'none',
+                                                    backgroundColor: '#fff'
+                                                }} placeholder="/" disabled/>
+                                                <Input disabled={true} style={{width: 60, textAlign: 'center', borderLeft: 0}}
+                                                       />
+                                            </InputGroup>
+                                        )
+                                    }
+                                </Col>
+                            </Row>
                         </Col>
                     </Row>
                     </Card>
