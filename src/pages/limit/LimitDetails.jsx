03d97a937b68843bf25b9dd21f8e64de93c55a7c
qiudong <qiu_dong@suixingpay.com>
2017-12-02 17:16:32 +0800

    统一页面

--- src/pages/limit/LimitDetails.jsx
+++ src/pages/limit/LimitDetails.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Input, Button, Form, Row, Col, Card} from 'antd'
 import {Containerization, InitComs, setTitle} from '../../common/PublicComponent';
 import InputComs from "../../components/InputComs";
+
 const InputGroup = Input.Group;
 
 const FormItem = Form.Item;
@@ -10,59 +11,63 @@ const FormItem = Form.Item;
 @Form.create()
 export default class LimitDetails extends React.Component {
 
+    state = {
+        isMerchant: false,
+    }
+
     initData = {
         merchantAttrData: [{
             labelName: '结算账户类型',
             labelValue: 'accountType',
-        },{
+        }, {
             labelName: '名单类型',
             labelValue: 'listType'
-        },{
+        }, {
             labelName: '是否优质商户',
             labelValue: 'isExcellentClient',
-        },{
+        }, {
             labelName: '行业大类',
             labelValue: 'industryCategory'
-        },{
+        }, {
             labelName: '细类',
             labelValue: 'detailCategory',
-        },{
+        }, {
             labelName: 'MCC',
             labelValue: 'mcc',
-        },{
+        }, {
             labelName: '是否小额双免',
             labelValue: 'isSmallExemption',
-        },{
+        }, {
             labelName: '是否有终端',
             labelValue: 'hasTerminal',
-        },{
+        }, {
             labelName: 'POS商户类型',
             labelValue: 'posClientType',
-        },{
+        }, {
             labelName: 'POS秒到等级',
             labelValue: 'posSecondRank',
-        },{
+        }, {
             labelName: 'POS结算周期',
             labelValue: 'posbalanceTime',
         }],
-        transactionAttrData:[{
+        transactionAttrData: [{
             labelName: '卡属性',
             labelValue: 'carType',
-        },{
+        }, {
             labelName: '卡介质',
             labelValue: 'media'
-        },{
+        }, {
             labelName: '消费方式',
             labelValue: 'consumeType'
-        },{
+        }, {
             labelName: '接触方式',
             labelValue: 'contactType'
         }],
-        scanData:[{
-                labelName: '扫码类型',
-                labelValue: 'scanType'
+        scanData: [{
+            labelName: '扫码类型',
+            labelValue: 'scanType'
         }],
-        modifyLimitData:[{
+        modifyLimitData: [{
             labelName: '单笔',
             labelValue: 'sigle',
             addonBefore: "金额",
@@ -98,8 +103,15 @@ export default class LimitDetails extends React.Component {
         ]
     }
 
+    componentWillMount() {
+        if (this.props.match.path.indexOf('merchant') > 0) {
+            this.setState({isMerchant: true})
+        }
+    }
+
     render() {
         const {getFieldDecorator} = this.props.form;
+        const {isMerchant} = this.state;
         const formItemLayout = {
             labelCol: {
                 xs: {span: 24},
@@ -118,23 +130,26 @@ export default class LimitDetails extends React.Component {
         const tailItemLayout = {
             xs: 24,
             sm: 24,    //平板的时候占一行，因为一行有24格
-            md:1       //在PC上的时候分一格  ，剩下的23格input组件来分
+            md: 1       //在PC上的时候分一格  ，剩下的23格input组件来分
         };
         return (
-            <div >
-                <div style={{textAlign: 'center', marginBottom: 10, marginTop: 10, fontSize: 16}} > <b>限额名称：POS商户对私结算限额</b> </div>
-                <Form >
+            <div>
+                <div style={{textAlign: 'center', marginBottom: 10, marginTop: 10, fontSize: 16}}> {
+                    isMerchant ? <b>商户编号：</b> : <b>限额名称：POS商户对私结算限额</b>
+                } </div>
+                <Form>
                     <Card title="商户属性：" bordered={true}>
                         <Row style={{marginTop: 10}}>
                             {
-                                this.initData.merchantAttrData.map((v,k)=>{
-                                    return(
+                                this.initData.merchantAttrData.map((v, k) => {
+                                    return (
                                         <Col  {...queryItemLayout}
-                                              key ={k} >
+                                              key={k}>
                                             {
-                                               getFieldDecorator(v.labelValue)(
-                                                   <InputComs  disabled={true} labelName={v.labelName} style={{width: 120}}/>
-                                               )
+                                                getFieldDecorator(v.labelValue)(
+                                                    <InputComs disabled={true} labelName={v.labelName}
+                                                               style={{width: 120}}/>
+                                                )
                                             }
                                         </Col>
                                     )
@@ -152,39 +167,41 @@ export default class LimitDetails extends React.Component {
                             </Col>
                             <Col>
                                 {
-                                        this.initData.transactionAttrData.map((v,k)=>{
+                                    this.initData.transactionAttrData.map((v, k) => {
 
-                                            return(
-                                                <Col {...queryItemLayout}
-                                                     key ={k}>
-                                                    {
-                                                        getFieldDecorator(v.labelValue)(
-                                                            <InputComs  disabled={true} labelName={v.labelName} style={{width: 120}}/>
-                                                        )
-                                                    }
-                                                </Col>
+                                        return (
+                                            <Col {...queryItemLayout}
+                                                 key={k}>
+                                                {
+                                                    getFieldDecorator(v.labelValue)(
+                                                        <InputComs disabled={true} labelName={v.labelName}
+                                                                   style={{width: 120}}/>
+                                                    )
+                                                }
+                                            </Col>
 
-                                                )
-                                        })
+                                        )
+                                    })
                                 }
 
                             </Col>
                         </Row>
-                        <Row >
+                        <Row>
                             <Col {...tailItemLayout}
                             >
                                 <span style={{marginTop: 16, display: 'inline-block'}}> 扫码交易:</span>
                             </Col>
                             <Col>
                                 {
-                                    this.initData.scanData.map((v,k)=>{
+                                    this.initData.scanData.map((v, k) => {
 
-                                        return(
+                                        return (
                                             <Col {...queryItemLayout}
-                                                 key ={k}>
+                                                 key={k}>
                                                 {
                                                     getFieldDecorator(v.labelValue)(
-                                                        <InputComs  disabled={true} labelName={v.labelName} style={{width: 120}}/>
+                                                        <InputComs disabled={true} labelName={v.labelName}
+                                                                   style={{width: 120}}/>
                                                     )
                                                 }
                                             </Col>
@@ -199,55 +216,58 @@ export default class LimitDetails extends React.Component {
                 </Form>
                 <Form>
                     <Card title={"修改限额值"} bordered={true}>
-                    <Row style={{marginTop: 10}}>
-                        {
-                            this.initData.modifyLimitData.map((v, k) => {
+                        <Row style={{marginTop: 10}}>
+                            {
+                                this.initData.modifyLimitData.map((v, k) => {
 
-                                    return (
-                                        <Col {...queryItemLayout}
-                                             key ={k}>
-                                            {
-                                                getFieldDecorator(v.labelValue)(
-                                                    <InputComs disabled={true} labelName={v.labelName} style={{width: 120}}
-                                                               addonBefore={v.addonBefore} addonAfter={v.addonAfter}/>
-                                                )
-                                            }
-                                        </Col>
-                                    )
-                                }
-                            )
-                        }
-                        <Col {...queryItemLayout}>
-                            <Row style={{marginTop: '10px'}}>
-                                <Col span={7}>
-                                    <span style={{marginTop: '5px', display: 'inline-block'}}>每笔／分钟:</span>
-                                </Col>
-                                <Col span={16}>
-                                    {
-                                        getFieldDecorator('limitType')(
-                                            <InputGroup>
-                                                <Input disabled={true} style={{width: 60, textAlign: 'center'}}
-                                                       />
-                                                <Input style={{
-                                                    width: 24,
-                                                    borderLeft: 0,
-                                                    pointerEvents: 'none',
-                                                    backgroundColor: '#fff'
-                                                }} placeholder="/" disabled/>
-                                                <Input disabled={true} style={{width: 60, textAlign: 'center', borderLeft: 0}}
-                                                       />
-                                            </InputGroup>
+                                        return (
+                                            <Col {...queryItemLayout}
+                                                 key={k}>
+                                                {
+                                                    getFieldDecorator(v.labelValue)(
+                                                        <InputComs disabled={true} labelName={v.labelName}
+                                                                   style={{width: 120}}
+                                                                   addonBefore={v.addonBefore} addonAfter={v.addonAfter}/>
+                                                    )
+                                                }
+                                            </Col>
                                         )
                                     }
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Row>
+                                )
+                            }
+                            <Col {...queryItemLayout}>
+                                <Row style={{marginTop: '10px'}}>
+                                    <Col span={7}>
+                                        <span style={{marginTop: '5px', display: 'inline-block'}}>每笔／分钟:</span>
+                                    </Col>
+                                    <Col span={16}>
+                                        {
+                                            getFieldDecorator('limitType')(
+                                                <InputGroup>
+                                                    <Input disabled={true} style={{width: 60, textAlign: 'center'}}
+                                                    />
+                                                    <Input style={{
+                                                        width: 24,
+                                                        borderLeft: 0,
+                                                        pointerEvents: 'none',
+                                                        backgroundColor: '#fff'
+                                                    }} placeholder="/" disabled/>
+                                                    <Input disabled={true}
+                                                           style={{width: 60, textAlign: 'center', borderLeft: 0}}
+                                                    />
+                                                </InputGroup>
+                                            )
+                                        }
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </Row>
                     </Card>
                 </Form>
-                <div style={{textAlign: 'center', marginBottom: 10, marginTop: 10}}><Button type={"primary"}>关闭</Button></div>
+                <div style={{textAlign: 'center', marginBottom: 10, marginTop: 10}}><Button type={"primary"}>关闭</Button>
+                </div>
             </div>
         )
     }
-    }
+}
 
--- src/pages/limit/OperationRecord.jsx
+++ src/pages/limit/OperationRecord.jsx
@@ -7,14 +7,24 @@ import {Containerization, setTitle} from '../../common/PublicComponent';
 @setTitle('操作记录')
 @Containerization()
 export default class OperationRecord extends React.Component {
+    state = {
+        isMerchant: false,
+    }
+
+    componentWillMount(){
+        if (this.props.match.path.indexOf('merchant') > 0) {
+            this.setState({isMerchant: true})
+        }
+    }
 
     render() {
+        const {isMerchant} = this.state;
         return (
             <div style={{ marginLeft: 10}}>
                 <p style={{fontSize: 14, marginBottom: 10}}>操作记录</p>
                 <Button style={{marginBottom: 20}}>返回</Button>
                 <div style={{marginBottom: 10}}>
-                    <h2>限额名称XXXXXXXXXXXXXXX</h2>
+                    {isMerchant? <h2>商户编号：</h2> : <h2>限额名称XXXXXXXXXXXXXXX</h2>}
                 </div>
                 <EditableTable/>
             </div>
