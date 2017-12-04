816e98ef90bd0c3037e0eacda4caf1d624c99ef7
qiudong <qiu_dong@suixingpay.com>
2017-12-04 20:49:57 +0800

    去掉冗余代码

--- src/router/merchant.config.js
+++ src/router/merchant.config.js
@@ -20,28 +20,22 @@ export const merchantLimitRouter = [
     {
         path: '/merchantlimit',
         exact: true,
-        breadcrumbName: '个性限额首页',
         component: (props) => BundleCom(props, LimitManager),
     }, {
         path: '/merchantlimit/+update/:id',
         exact: true,
-        breadcrumbName: '修改个性限额',
-
         component: props => BundleCom(props, LimitUpdate)
     }, {
         path: '/merchantlimit/+details/:id',
         exact: true,
-        breadcrumbName: '个性限额详情',
         component: (props) => BundleCom(props, LimitDetails),
     }, {
         path: '/merchantlimit/+operationrecoerd/:id',
         exact: true,
-        breadcrumbName: '个性限额操作记录',
         component: (props) => BundleCom(props, OperationRecord),
     },{
         path: '/merchantlimit/+add/:id',
         exact: true,
-        breadcrumbName: '新增个性限额',
         component: props => BundleCom(props, NewLimitModel)
     },
 ]
