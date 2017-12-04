594a5d1d56c4ea9c541998bc13f2ff4cbc578957
jerryxu <13048699388@163.com>
2017-12-04 20:32:15 +0800

    [操作记录]去掉无用的元算

--- src/pages/limit/OperationRecord.jsx
+++ src/pages/limit/OperationRecord.jsx
@@ -20,8 +20,7 @@ export default class OperationRecord extends React.Component {
         const {isMerchant} = this.state;
         return (
             <div style={{ marginLeft: 10}}>
-                <p style={{fontSize: 14, marginBottom: 10}}>操作记录</p>
-                <Button style={{marginBottom: 20}}>返回</Button>
+
                 <div style={{marginBottom: 10}}>
                     {isMerchant? <h2>商户编号：</h2> : <h2>限额名称XXXXXXXXXXXXXXX</h2>}
                 </div>
