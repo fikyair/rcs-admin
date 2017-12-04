809e606463863752811828760e5b182eb97de81f
qiudong <qiu_dong@suixingpay.com>
2017-12-04 10:42:00 +0800

    luyou

--- src/router/Routers.jsx
+++ src/router/Routers.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Route, Redirect} from 'react-router-dom';
+import {BrowserRouter, Route, Redirect} from 'react-router-dom';
 import MenuComs from '../components/MenuComs';
-import { InitComs } from  '../common/PublicComponent';
-import { renderRoutes } from 'react-router-config';
-import { commontLimtRouter } from './common.config.js';
-import { merchantLimitRouter} from "./merchant.router.js";
+import {InitComs} from '../common/PublicComponent';
+import {renderRoutes} from 'react-router-config';
+import {commontLimtRouter} from './common.config.js';
+import {merchantLimitRouter} from "./merchant.router.js";
 import {root} from "./root.config.js"
 import BreadCrumbComs from '../components/BreadCrumbComs'
 
@@ -16,34 +16,34 @@ const routes = [
                 {renderRoutes(route.routes, {someProp: 'these extra props are optional'})}
             </div>
         ),
-        routes: [...commontLimtRouter, ...merchantLimitRouter, root]
+        routes: [...commontLimtRouter, ...merchantLimitRouter, ...root]
     }
 ]
 
 export default (
-  <div>
-    <BrowserRouter basename={''} >
-      <div>
-          <InitComs />
-          <MenuComs />
-          <BreadCrumbComs />
-          {renderRoutes(routes)}
-      </div>
-    </BrowserRouter>
-  </div>
+    <div>
+        <BrowserRouter basename={''}>
+            <div>
+                <InitComs/>
+                <MenuComs/>
+                <BreadCrumbComs/>
+                {renderRoutes(routes)}
+            </div>
+        </BrowserRouter>
+    </div>
 )
 
-const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route {...rest} render={props => (
-    isLogin ? (
-      <Component {...props}/>
-    ) : (
-      <Redirect to={{
-        pathname: '/login',
-        state: { from: props.location }
-      }}/>
-    )
-  )}/>
+const PrivateRoute = ({component: Component, ...rest}) => (
+    <Route {...rest} render={props => (
+        isLogin ? (
+            <Component {...props}/>
+        ) : (
+            <Redirect to={{
+                pathname: '/login',
+                state: {from: props.location}
+            }}/>
+        )
+    )}/>
 )
 
 
--- src/router/root.config.js
+++ src/router/root.config.js
@@ -1,17 +1,28 @@
-import LimitManager from "../pages/limit/LimitManager";
-import Login from "../pages/login/Login";
+import React from 'react'
+import LimitManager from "bundle-loader?lazy!../pages/limit/LimitManager";
+import Login from "bundle-loader?lazy!../pages/login/Login";
+import Bundle from './DynamicRoute';
+
+
+const BundleCom = (props, loader) => {
+    return (
+        <Bundle load={loader}>
+            {(Com) => <Com {...props}/>}
+        </Bundle>
+    )
+}
 
 export const root = [
-{
-    path: '/',
+    {
+        path: '/',
         exact: true,
-    component: (props) => BundleCom(props, LimitManager),
-}, {
-    path: '/login',
-        exact: true,
-        component: (props) => BundleCom(props, Login),
-}, {
-    path: '/login/:id',
+        component: (props) => BundleCom(props, LimitManager),
+    }, {
+        path: '/login',
         exact: true,
         component: (props) => BundleCom(props, Login),
-}, ]
+    }, {
+        path: '/login/:id',
+        exact: true,
+        component: (props) => BundleCom(props, Login),
+    },]
