55a3e3f2c577c4834ab831faae9abba5d333d08d
fikyair <shiming_xsm@163.com>
2017-12-21 20:41:00 +0800

    [更新]我要找房

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,17 +1,77 @@
 import React from 'react';
+import '../style/about.less';
+import {Card} from 'antd';
 
 export default class Index extends React.Component{
 
 
+
     render(){
         return(
-            <div>
-
-               <button>sdsd</button>
-
-            </div>
-        )
-
+                <div className="wrapper clearfix" style={{marginTop: 50}} >
+                    <Card className="fl roomintro" noHovering={true}   >
+                        <div className="hp_usbox">
+                            <ul>
+                                <li className="active">
+                                    <a href="#">关于爱家</a>
+                                </li>
+                                <li className="">
+                                    <a href="#">联系爱家</a>
+                                </li>
+                                <li className="">
+                                    <a href="#">加入爱家</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </Card>
+                    <div className="fr roomcenter">
+                        <div className="hp_title">
+                            <div className="hp_t_h">
+                                <h1>关于蛋壳</h1>
+                            </div>
+                            <span className="hp_t_line"></span>
+                        </div>
+                        <div className="hp_txtbox">
+                            <p>
+                                我们不建造房子，<br/>
+                                但我们改造房间。<br/>
+                                但我们改造房间。<br/>
+                                但我们给您五星级睡眠。<br/>
+                            </p>
+                                <p>
+                                    我们不是星巴克，<br/>
+                                    但我们为您提供高速免费Wifi。<br/>
+                                    我们不开发房地产，<br/>
+                                    但我们提供私人定制的居住空间。<br/>
+                                    我们不是房产中介公司，<br/>
+                                    我们是互联网公司 ~ 爱家房屋。<br/>
+                                </p>
+                                <p>
+                                    爱家房屋是紫梧桐（北京）资产管理有限公司旗下的高端白领公寓品牌，专注于为白领打造高品质租住生活，并以精致的装修、品牌家居家电配置、优质的服务快速赢得了租客的青睐。公司于2015年1月在北京成立，正式进入O2O租房市场，并迅速成为互联网租房行业的一匹黑马！
+                                </p>
+                                <p>
+                                    <strong>爱家房屋不是中介</strong>，不是传统意义上的房屋租赁公司，爱家房屋是互联网公司，是资产管理公司。依托于“互联网+房产+金融”的发展模式，爱家房屋已获得A+轮融资，注资千万级美元。
+                                </p>
+                                <p>
+                                    目前，公司已从创始初期的11人迅速发展到现在的400多人，业务已从北京拓展到深圳、上海等地。团队成员大多是有互联网从业经验的高端人才，涉及技术、市场、销售、运营等多个领域。人才聚而发展始，公司正以强劲的发展态势迅速崛起！
+                                </p>
+                        </div>
+                    </div>
+                    <div className="footer">
+                        <div className="footcontainer">
+                            <div className="fl contai">
+                                <a href="tel:400-018-8090" className="m_keep" id="click_bottom_tel"><i></i><span className="telphoneview">客服热线：400-018-8090</span><span className="telphonepcview">客服热线：400-818-5656</span></a>
+                                <a href="#">关于蛋壳</a><span>·</span>
+                                <a href="#">联系蛋壳</a><span>·</span>
+                                <a href="#">加入蛋壳</a><span>·</span>
+                                <span>关注我们</span>
+                            </div>
+                            <div className="fr copyt">
+                                © 2016 蛋壳公寓 京ICP备15009197号
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        );
     }
-
 }
--- src/pages/Rent.jsx
+++ src/pages/Rent.jsx
@@ -1,67 +1,131 @@
 import React from 'react';
 import {Card} from 'antd';
-import BreadCrumbComs from "../components/BreadCrumbComs";
 import '../style/rent.less';
 
 export default class Index extends React.Component {
 
+    state = {
+        display: false,
+        checkedFlag: true,
+        currentActive:null,
+    }
 
+    mockData = {
+        cityData: [
+            {
+                name: '东城区', value: "11"
+            },
+            {
+                name: '朝阳区', value: "12"
+            },{
+                name: '西城区', value: '13'
+            },
+        ],
+        streetData: [
+            {
+                name: '宜山路', value: "1101"
+            },
+            {
+                name: '漕宝路', value: "1102"
+            }, {
+                name: '龙漕路', value: "1103"
+            }, {
+                name: '桂林路', value: "1104"
+            }, {
+                name: '上海体育场', value: "1105"
+            }, {
+                name: '徐家汇', value: "1106"
+            }, {
+                name: '虹漕路', value: "1107"
+            }, {
+                name: '上海游泳馆', value: "1108"
+            },
+        ]
+    }
+
+    cityClick(k){
+        this.setState({display: true,currentActive:k, checkedFlag: false});
+    }
+    limitClick(){
+        this.setState({checkedFlag: !this.state.checkedFlag, currentActive: null, display: false});
+    }
     render() {
+        const city = this.mockData.cityData;
+        const street = this.mockData.streetData;
+
         return (
             <div>
-                    <Card className = "wrapper" style={{marginTop: 50}}>
-                        <div className="filter_options">
-                            <dl className="dl_lst">
-                                <dt>区域：</dt>
-                                <dd>
+                <Card className="wrapper" style={{marginTop: 50}} noHovering={true}>
+                    <div className="filter_options">
+                        <dl className="dl_lst">
+                            <dt>区域：</dt>
+                            <dd>
+                                <div className="option_list">
+                                    <a onClick={() => this.limitClick()} className={this.state.checkedFlag?'onlist':''}>不限</a>
+                                    {
+                                        city.map((data,k) => {
+                                            return(
+                                            <a key={k} onClick={(e)=>this.cityClick(k)} className ={this.state.currentActive == k?'onlist':''}>{data.name}</a>
 
-                                    <div className="option_list">
-                                        <a href="#" className="onlist">不限</a>
+                                            )
+                                        })
+                                    }
 
-                                        <a href="#" className="">东城区</a>
-                                    </div>
+                                </div>
+                                <div className="sub_option_list" style={this.state.display?{display: 'block'}:　{display: 'none'}}>
+                                    {
+                                        street.map((data,k) => {
+                                            console.log("sss", data);
+                                            return (
+                                                <a key={k} href="#" className="">{data.name}</a>
 
-                                </dd>
-                            </dl>
+                                            )
+                                        })
+                                    }
 
-                            <dl className="dl_lst">
-                                <dt>租金：</dt>
-                                <dd>
-                                    <div className="option_list">
-                                        <a href="#" className="onlist">不限</a>
-                                        <a href="#" className="">
-                                            2000元以下
-                                        </a>
-                                        <a href="#" className="">
-                                            2000~2500元
-                                        </a>
-                                        <a href="#" className="">
-                                            2500~3000元
-                                        </a>
-                                        <a href="#" className="">
-                                            3000~3500元
-                                        </a>
-                                        <a href="#" className="">
-                                            3500元以上
-                                        </a>
-                                    </div>
-                                </dd>
-                            </dl>
-                            <dl className="dl_lst last" style={{display: 'none'}}>
-                                <dt>居室：</dt>
-                                <dd>
-                                    <div className="option_list">
-                                        <a href="#" className="onlist">不限</a>
-                                        <a href="#" className="">一居室</a>
-                                        <a href="#" className="">两户合租</a>
-                                        <a href="#" className="">三户合租</a>
-                                        <a href="#" className="">四户及以上合租</a>
-                                    </div>
-                                </dd>
-                            </dl>
-                        </div>
-                    </Card>
-                <Card className="roomlist wrapper">
+                                </div>
+                            </dd>
+                        </dl>
+
+                        <dl className="dl_lst">
+                            <dt>租金：</dt>
+                            <dd>
+                                <div className="option_list">
+                                    <a href="#" className="onlist">不限</a>
+                                    <a href="#" className="">
+                                        2000元以下
+                                    </a>
+                                    <a href="#" className="">
+                                        2000~2500元
+                                    </a>
+                                    <a href="#" className="">
+                                        2500~3000元
+                                    </a>
+                                    <a href="#" className="">
+                                        3000~3500元
+                                    </a>
+                                    <a href="#" className="">
+                                        3500元以上
+                                    </a>
+                                </div>
+                            </dd>
+                        </dl>
+
+                        <dl className="dl_lst last" style={{display: 'none'}}>
+                            <dt>居室：</dt>
+                            <dd>
+                                <div className="option_list">
+                                    <a href="#" className="onlist">不限</a>
+                                    <a href="#" className="">一居室</a>
+                                    <a href="#" className="">两户合租</a>
+                                    <a href="#" className="">三户合租</a>
+                                    <a href="#" className="">四户及以上合租</a>
+                                </div>
+                            </dd>
+                        </dl>
+                    </div>
+                </Card>
+                <Card className="roomlist wrapper" noHovering={true}>
                     <div className="r_ls">
 
                         <a href="#" className="ck_on">默认</a>
