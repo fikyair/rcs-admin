65df9e98320ac17495aaea953728583efd60573c
fikyair <shiming_xsm@163.com>
2018-04-16 22:37:49 +0800

    首页房屋初始化

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -32,7 +32,7 @@ export default class Index extends React.Component {
 
     componentWillMount (){
         //查询所有的房源信息
-        this.props.dispatch(get_flat_all()).then(() => {
+        this.props.dispatch(get_flat_all('北京市')).then(() => {
 
             if(this.props.flatAllDataInit != null){
                 let arr = [];
